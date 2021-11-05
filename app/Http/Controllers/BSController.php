b6d1c6570f4db3bb808ef2c66d21010b2b9cce5e
Will CD <chavezdiaz4@gmail.com>
2021-11-05 15:24:55 -0500

    Creacion del sistema de batalla y eventos en BSController

--- app/Http/Controllers/BSController.php
+++ app/Http/Controllers/BSController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Hero;
+use App\Models\Enemy;
 
 //No usar resource porque ya no se basara en un modelo
 class BSController extends Controller
@@ -10,6 +12,68 @@ class BSController extends Controller
 
     public function index()
     {
-        return view('admin.bs.index'); //vista de batallas
+        $hero = Hero::find(1)->first(); //Mostrar el primer heroe
+        $enemy = Enemy::find(1)->first(); //Mostrar el primer enemigo
+
+        //arreglo en la base de datos
+        $events = [];
+
+        //Mientras los enemigos sigan vivos
+        while ($hero->hp > 0 && $enemy->hp > 0) {
+            $luck = random_int(0, 100); //suerte valor random
+
+            if ($luck >= 50) { //Si es mayor a 50 el heroe pega
+                $hp = $enemy->def - $hero->atq; //la defensa absorve el golpe
+
+                if ($hp < 0) {
+                    $enemy->hp -= $hp * -1; //Resta la vida del enemigo_acum
+                }
+
+                //dd($enemy->hp); //veremos la vida que le queda al enemigo
+
+                //Array de eventos
+                if ($enemy->hp > 0) { //si el enemigo tiene vida
+                    $ev = [
+                        "winner" => "hero",
+                        "text" => $hero->name . " hizo un daño de " . $hero->atq . " a " . $enemy->name
+                    ];
+                } else {
+                    $ev = [
+                        "winner" => "hero",
+                        "text" => $hero->name . " acabó con la vida de " . $enemy->name
+                    ];
+                }
+
+                //print("Enemy HP: " . $enemy->hp . "<br/>");
+
+            } else {
+                $hp = $hero->def - $enemy->atq;
+
+                if ($hp < 0) {
+                    $hero->hp -= $hp * -1; //Decremento de vida para heroe
+                }
+
+                if ($hero->hp > 0) {
+                    $ev = [
+                        "winner" => "enemy",
+                        "text" => $hero->name . " recibio un daño de " . $enemy->atq . " puntos por " . $enemy->name
+                    ];
+                } else {
+                    $ev = [
+                        "winner" => "enemy",
+                        "text" => $hero->name . " fue asesinado por " . $enemy->name
+                    ];
+                }
+                //print("Hero HP: " . $hero->hp . "<br/>");
+            }
+
+            array_push($events, $ev); //setea datos en el arreglo eventos de enemigo y heroe
+        }
+
+        dd($events);
+
+        //dd($enemy); //Muestra los atributos en linea del model en pantalla
+
+        //return view('admin.bs.index'); //vista de batallas
     }
 }
--- resources/views/admin/bs/index.blade.php
+++ resources/views/admin/bs/index.blade.php
@@ -6,16 +6,16 @@
         <h1>Sistema de Batalla</h1>
     </div>
 
+    <!--Sistemas de columnas col- 5 2 5: total de 12-->
     <div class="row text-center text-white mt-2">
-        <div class="col bg-primary">
+        <div class="col-5 bg-primary">
             <h2>HERO</h2>
         </div>
-
         <div class="col bg-warning">
             <h2>VS</h2>
         </div>
 
-        <div class="col bg-danger">
+        <div class="col-5 bg-danger">
             <h2>ENEMY</h2>
         </div>
     </div>
