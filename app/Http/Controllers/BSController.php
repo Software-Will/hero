198b79d8d6423181d7f8841d478532e16ac86402
Will CD <chavezdiaz4@gmail.com>
2021-11-05 16:20:32 -0500

    Implementacion de jugada manual

--- app/Http/Controllers/BSController.php
+++ app/Http/Controllers/BSController.php
@@ -12,8 +12,16 @@ class BSController extends Controller
 
     public function index()
     {
-        $hero = Hero::find(1)->first(); //Mostrar el primer heroe
-        $enemy = Enemy::find(1)->first(); //Mostrar el primer enemigo
+
+        dd($this->runManualBattle(1, 1));
+        return view('admin.bs.index', $this->runAutoBatle(1, 1)); //vista de batallas y llamado de funcion que retorna datos
+    }
+
+    //Metodo para encapsulamiento de la funcion de sistema de batallas
+    public function runAutoBatle($heroId, $enemyId)
+    {
+        $hero = Hero::find($heroId)->first(); //Mostrar el heroe segun id
+        $enemy = Enemy::find($enemyId)->first(); //Mostrar el enemigo segun id
 
         //arreglo en la base de datos
         $events = [];
@@ -81,14 +89,69 @@ class BSController extends Controller
             array_push($events, $ev); //setea datos en el arreglo eventos de enemigo y heroe
         }
 
-        //dd($events);
-
-        //dd($enemy); //Muestra los atributos en linea del model en pantalla
-
-        return view('admin.bs.index', [
+        return [
             "events" => $events,
             "heroName" => $hero->name,
             "enemyName" => $enemy->name
-        ]); //vista de batallas y envio de eventos
+        ];
+    }
+
+    //Metodo para batalla manual
+    public function runManualBattle($heroId, $enemyId)
+    {
+        $hero = Hero::find($heroId)->first(); //Mostrar el heroe segun id
+        $enemy = Enemy::find($enemyId)->first(); //Mostrar el enemigo segun id
+
+        $luck = random_int(0, 100); //suerte valor random
+
+        if ($luck > 50) {
+            $hp = $enemy->def - $hero->atq; //la defensa absorve el golpe
+
+            if ($hp < 0) {
+                $enemy->hp -= $hp * -1; //Resta la vida del enemigo_acum
+            }
+
+            //Array de eventos
+            if ($enemy->hp > 0) { //si el enemigo tiene vida
+                return [
+                    "winner" => "hero",
+                    "text" => $hero->name . " hizo un daño de " . $hero->atq . " a " . $enemy->name
+                ];
+            } else {
+                return [
+                    "winner" => "hero",
+                    "text" => $hero->name . " acabó con la vida de " . $enemy->name . " ganando " . $enemy->xp . " xp"
+                ];
+
+                $hero->xp = $hero->xp + $enemy->xp; //aumentando la experiencia del heroe
+
+                if ($hero->xp >= $hero->level->xp) { //si es mayor o igual a los xp de la table levels
+                    $hero->xp = 0; //reseteo de nivel para siguiente nivel
+                    $hero->level_id += 1; //aumento de nivel
+                }
+
+                //dd($hero->xp);
+
+                $hero->save(); //guardamos xp
+            }
+        } else { //caso gane enemigo
+            $hp = $hero->def - $enemy->atq;
+
+            if ($hp < 0) {
+                $hero->hp -= $hp * -1; //Decremento de vida para heroe
+            }
+
+            if ($hero->hp > 0) {
+                return [
+                    "winner" => "enemy",
+                    "text" => $hero->name . " recibio un daño de " . $enemy->atq . " puntos por " . $enemy->name
+                ];
+            } else {
+                return [
+                    "winner" => "enemy",
+                    "text" => $hero->name . " fue asesinado por " . $enemy->name
+                ];
+            }
+        }
     }
 }
